18b8785f95a1890e7a0b52b976f62340f182145d
yhostinCTC <yhostin.ollachica@crackthecode.la>
2023-03-06 11:36:36 -0500

    add: form para crear peliculas

---
+++ app/forms/form-card.js
@@ -0,0 +1,19 @@
+import { savePeliculas } from "../config.js";
+
+export const formCardListener = () =>{
+    let formCard = document.getElementById('form_card')
+    formCard.addEventListener('submit', async (evento) =>{
+        evento.preventDefault();
+        let director = formCard['inputDirector'].value;
+        let titulo = formCard['inputTitulo'].value;
+        let genero = formCard['inputGenero'].value;
+        console.log(director, titulo, genero)
+
+        try {
+            let response = await savePeliculas(director, titulo, genero)
+            console.log(response)
+        } catch (error) {
+            console.log("ERROR FATAL, NO FUNCIONO, VAMONOS A COMER MEJOR")
+        }
+    })
+}
