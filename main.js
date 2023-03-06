9012c582cbc9d5e062c590c6ba5ddf43cb64d7ae
yhostinCTC <yhostin.ollachica@crackthecode.la>
2023-03-06 11:37:03 -0500

    add:cartas y formulario

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
-import { auth } from "./app/config.js"
-import { setupPosts } from "./data.js"
+import { auth, getPeliculas } from "./app/config.js"
+import { setupPosts } from "./utils/data.js"
 
+
+import { formCardListener } from "./app/forms/form-card.js"
 import './app/forms/form-login.js'
 import './app/forms/form-logout.js'
 import './app/forms/form-registro.js'
 
+import { card } from "./utils/card.js"
+
 onAuthStateChanged(auth, async (user) => {
     if (user) {
       //loginCheck(user);
-      
-      try {     
-        const mensaje = "Iniciaste sesion";
-  
-        setupPosts(mensaje); 
+      let mensaje = "";
+      try {
+        let peliculas = await getPeliculas()
+        peliculas.forEach(pelicula => {
+          mensaje+= card(pelicula.data());
+        });
+        // 
+        setupPosts(mensaje);
+        formCardListener();
       } catch (error) {
         console.log(error)
       }
